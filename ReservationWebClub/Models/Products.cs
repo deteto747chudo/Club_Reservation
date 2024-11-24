1ad68d1cddd09a3faef5b8c3684b9c885f23868a
denislav <127776832+deteto747chudo@users.noreply.github.com>
2024-11-24 20:41:39 +0200

    Update Products.cs

--- ReservationWebClub/Models/Products.cs
+++ ReservationWebClub/Models/Products.cs
@@ -1,1 +1,10 @@
-
+namespace ReservationWebClub.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
