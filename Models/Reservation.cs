2d6467661541b2d0645defeb58c0f612e30ef4cb
denislav <127776832+deteto747chudo@users.noreply.github.com>
2024-11-28 21:20:25 +0200

    Add files via upload

---
+++ Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace ReservationWebClub.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int TableId { get; set; }
+        public Table Table { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservationWebClub.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public int SeatCount { get; set; }
+        public bool IsSmokingAllowed { get; set; }
+    }
+
+}
